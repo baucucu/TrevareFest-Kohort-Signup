592c5e2912b8e1ec9df8157a473b4f7caafa1106
Alex Raduca <alexandru.raduca@gmail.com>
2021-07-13 18:18:27 +0300

    Update Step2.js

--- src/Step2.js
+++ src/Step2.js
@@ -71,7 +71,7 @@ const Step2 = (props) => {
   async function getKohorts() {
     base("Directory: Kohorts")
       .select({
-        view: state.email.indexOf("@") > -1 ? "Available" : "Crew"
+        view: state?.email ? "Crew" : "Available"
       })
       .firstPage(function (err, records) {
         if (err) {
